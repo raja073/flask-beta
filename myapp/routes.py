9ab02572f334facc99c6a86746c1347077a71026
rach62 <raja.asokan@gmail.com>
2018-03-02 15:56:29 +0100

    added templates

--- myapp/routes.py
+++ myapp/routes.py
@@ -1,6 +1,18 @@
 from myapp import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Hello World!"
+	user = {'username': 'Raja'}
+	posts = [
+		{
+			'author': {'username': 'John'},
+			'body': 'Beautiful day in kattoor'
+		},
+		{
+			'author': {'username': 'Susan'},
+			'body': 'The nazis sucks'
+		}
+	]
+	return render_template('index.html', title='Home', user=user, posts=posts)
