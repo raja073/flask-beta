185a8ff99758a95ce84bf96887313243adaec12f
rach62 <raja.asokan@gmail.com>
2018-03-04 11:24:01 +0100

    added database and migrations

--- flask_beta.py
+++ flask_beta.py
@@ -1,1 +1,7 @@
-from myapp import app
+from myapp import app, db
+from myapp.models import User, Post
+
+
+@app.shell_context_processor
+def make_shell_context():
+	return {'db': db, 'User': User, 'Post': Post}
